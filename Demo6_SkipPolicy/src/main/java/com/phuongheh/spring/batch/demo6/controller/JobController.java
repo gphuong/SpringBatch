35e99693d86260e4045e4a1050f1a02281f889d6
Gian Phuong <phuongheh@gmail.com>
2023-04-27 23:21:46 +0700

    Skip Policy batch practice

---
+++ Demo6_SkipPolicy/src/main/java/com/phuongheh/spring/batch/demo6/controller/JobController.java
@@ -0,0 +1,24 @@
+package com.phuongheh.spring.batch.demo6.controller;
+
+import com.phuongheh.spring.batch.demo6.runner.JobRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/run")
+public class JobController {
+
+    private JobRunner jobRunner;
+
+    @Autowired
+    public JobController(JobRunner jobRunner) {
+        this.jobRunner = jobRunner;
+    }
+
+    @RequestMapping(value = "/job")
+    public String runJob() {
+        jobRunner.runBatchJob();
+        return String.format("Job Demo6 submitted successfully.");
+    }
+}
